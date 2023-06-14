886f472f3f338f675fc7524bb8ba2e7c23e2092f
Pam Gaiguer <pamella.gaiguer@gmail.com>
2023-06-14 17:28:16 -0300

    starting coding test; routing added and styles from PrimeNg

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { MovieListComponent } from './movie-list/movie-list.component'
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'movies', component: MovieListComponent }]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
